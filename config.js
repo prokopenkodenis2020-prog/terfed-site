6d7ef3e28fd0b2edfc1f4a78742b46c633a6bd65
prokopenkodenis2020-prog <prokopenkodenis2020@gmail.com>
2026-08-10 15:58:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 window.TERFED_CONFIG = {
   VIDEO_ID: "tsgqYKhNUSQ",
+
   YOUTUBE_CHANNEL: "https://www.youtube.com/@terfed_ua",
+
   CATALOG_PDF: "assets/docs/TERFED-catalog-2026.pdf",
+
   EMAIL: "agrokomplekt@ukr.net",
+
   PHONE_1: "+380675502525",
+
   PHONE_2: "+380672172950",
+
   COMPANY: "ФОП Федоренко В.М.",
-  ADDRESS: "09112, м. Біла Церква, вул. Сквирське шосе, 29, Київська область, Україна"
+
+  ADDRESS:
+    "09112, м. Біла Церква, вул. Сквирське шосе, 29, Київська область, Україна"
 };
